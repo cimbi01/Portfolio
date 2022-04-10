2a2166ffc1baac076d9f2121e64cbe5789883986
Czimbal Balázs <Cimbi@cimbi.cimbi>
2022-04-10 18:30:55 +0200

    JobData CalculateSuitability implemented JobData refactored JobDataTest added

---
+++ Jobs.Test/JobDataTest.cs
@@ -0,0 +1,418 @@
+﻿using Jobs.Data;
+using Jobs.Data.WorkingPerson.Employee;
+using Jobs.Data.WorkingPerson.Employer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jobs.Test
+{
+    [TestFixture]
+    class JobDataTest
+    {
+        private Employee employee;
+        private JobData jobData;
+
+        private static Dictionary<string, int>[][] testCasesForAllMatch =
+        {
+            /*
+             * All employee rate matches needed rate
+             * Needed and employee skills are equals
+             */
+            new Dictionary<string, int>[]
+            { 
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                }
+            },
+            /*
+             * All employee rate matches needed rate
+             * More employee skills than needed
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "c", 1 }, {"a", 2 }, {"b", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "b", 3 }, {"c", 1 }, {"a", 2}, {"d", 4 }
+                }
+            }
+        };
+
+        private static Dictionary<string, int>[][] testCasesForNoMatch =
+        {
+            /*
+             * No needed and employee skills are equals
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }
+                },
+                new Dictionary<string, int>()
+                {
+                   {"c", 3 }, { "d", 2}
+                }
+            },
+        };
+
+        private static Dictionary<string, int>[][] testCasesForAllMatchHigher =
+        {
+            /*
+             * Needed and employee skills are equals
+             * Some employee rate higher than needed
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 2 }, {"b", 2 }, {"c", 4 }
+                }
+            },
+
+            /*
+             * More employee skills than needed
+             * Some employee rate higher than needed
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 2 }, {"b", 2 }, {"c", 4 }, { "d", 4}
+                }
+            },
+        };
+
+        private static Dictionary<string, int>[][] testCasesForAllMatchLower=
+        {
+            /*
+             * More needed skills than employee
+             * All employee rate matches needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "d", 4}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                }
+            },
+            /*
+             * More needed skills than employee
+             * Some employee rate lower than needed
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "d", 4}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 3 }
+                }
+            },
+            /*
+             * Needed and employee skills are equals
+             * Some employee rate lower than needed
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 3 }
+                }
+            },
+            /*
+             * Some employee skills not matching needed, some needed skills not matching employee
+             * Some employee rate lower than needed
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "e", 5}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 3 }, { "d", 4}
+                }
+            },
+            /*
+             * Some employee skills not matching needed, some needed skills not matching employee
+             * All employee rate matches needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "e", 5}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "d", 4}
+                }
+            },
+            /*
+             * Some employee skills not matching needed, some needed skills not matching employee
+             * Some employee rate matches needed rate
+             * Some employee rate lower than needed rate
+             * Some employee rate higher than needed rate
+            */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "e", 5}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 4 }, { "d", 4}
+                }
+            },
+            /*
+             * Some employee skills not matching needed, some needed skills not matching employee
+             * Some employee rate lower than needed rate
+             * Some employee rate higher than needed rate
+            */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "e", 5}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 2 }, {"b", 1 }, {"c", 4 }, { "d", 4}
+                }
+            },
+            /*
+             * Some employee skills not matching needed, some needed skills not matching employee
+             * Some employee rate matches needed rate
+             * Some employee rate lower than needed rate
+            */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "e", 5}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 3 }, { "d", 4}
+                }
+            },
+             /* 
+             * More needed skills than employee
+             * Some employee rate matches needed rate
+             * Some employee rate lower than needed rate
+             * Some employee rate higher than needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "d", 4}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 4 }
+                }
+            },
+             /*
+             * More needed skills than employee
+             * Some employee rate matches needed rate
+             * Some employee rate lower than needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "d", 4}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 1 }
+                }
+            }, 
+             /*
+              * More needed skills than employee
+             * Some employee rate lower than needed rate
+             * Some employee rate higher than needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }, { "d", 4}
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 2 }, {"b", 1 }, {"c", 4 }
+                }
+            }, 
+             /* 
+             * Needed and employee skills are equals
+             * Some employee rate matches needed rate
+             * Some employee rate lower than needed rate
+             * Some employee rate higher than needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 1 }, {"c", 4 }
+                }
+            },
+             
+             /*
+              * Needed and employee skills are equals
+             * Some employee rate matches needed rate
+             * Some employee rate lower than needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 1 }
+                }
+            },
+             
+             /*
+              * Needed and employee skills are equals
+             * Some employee rate lower than needed rate
+             * Some employee rate higher than needed rate
+             */
+            new Dictionary<string, int>[]
+            {
+                new Dictionary<string, int>()
+                {
+                    { "a", 1 }, {"b", 2 }, {"c", 3 }
+                },
+                new Dictionary<string, int>()
+                {
+                    { "a", 3 }, {"b", 1 }, {"c", 4 }
+                }
+            },
+        };
+
+
+        private void Init(Dictionary<string, int> neededKnowledgeRanges, Dictionary<string, int> employeeKnowledgeRanges)
+        {
+            this.InitEmployee(employeeKnowledgeRanges);
+            this.InitJobData(neededKnowledgeRanges);
+        }
+
+        private void InitEmployee(Dictionary<string, int> employeeKnowledgeRanges)
+        {
+            string testName = "TestName";
+            employee = new Employee(testName);
+            List<Skill> employeeSkills = new List<Skill>();
+            for (int i = 0; i < employeeKnowledgeRanges.Keys.Count; i++)
+            {
+                string skillName = employeeKnowledgeRanges.Keys.ElementAt(i);
+                int range = employeeKnowledgeRanges[skillName];
+                string details = "details" + Convert.ToString(range);
+                employeeSkills.Add(
+                    new Skill(skillName, details, range));
+            }
+            employee.ProfessionData.Skills.AddRange(employeeSkills);
+        }
+
+        private void InitJobData(Dictionary<string, int> neededKnowledgeRanges)
+        {
+            Employer employer = new Employer("Employer1");
+            jobData = new JobData("job1", employer);
+
+            List<Skill> neededSkills = new List<Skill>();
+            for (int i = 0; i < neededKnowledgeRanges.Keys.Count; i++)
+            {
+                string skillName = neededKnowledgeRanges.Keys.ElementAt(i);
+                int range = neededKnowledgeRanges[skillName];
+                string details = "details" + Convert.ToString(range);
+                neededSkills.Add(
+                    new Skill(skillName, details, range));
+            }
+            jobData.NeededSkills.AddRange(neededSkills);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(JobDataTest.testCasesForAllMatch))]
+        public void CalculateSuitabilityAllMatchTest(Dictionary<string, int> neededKnowledgeRanges, Dictionary<string, int> employeeKnowledgeRanges)
+        {
+            this.Init(neededKnowledgeRanges, employeeKnowledgeRanges);
+            double suitability = jobData.CalculateSuitability(employee);
+            double expectedSuitability = 1;
+
+            Assert.AreEqual(expectedSuitability, suitability);
+            Assert.GreaterOrEqual(suitability, 0);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(JobDataTest.testCasesForNoMatch))]
+        public void CalculateSuitabilityNoMatchTest(Dictionary<string, int> neededKnowledgeRanges, Dictionary<string, int> employeeKnowledgeRanges)
+        {
+            this.Init(neededKnowledgeRanges, employeeKnowledgeRanges);
+            double suitability = jobData.CalculateSuitability(employee);
+            double expectedSuitability = 0;
+
+            Assert.AreEqual(expectedSuitability, suitability);
+            Assert.GreaterOrEqual(suitability, 0);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(JobDataTest.testCasesForAllMatchHigher))]
+        public void CalculateSuitabilityAllMatchOrHigherTest(Dictionary<string, int> neededKnowledgeRanges, Dictionary<string, int> employeeKnowledgeRanges)
+        {
+            this.Init(neededKnowledgeRanges, employeeKnowledgeRanges);
+            double suitability = jobData.CalculateSuitability(employee);
+
+            Assert.GreaterOrEqual(suitability, 1);
+            Assert.GreaterOrEqual(suitability, 0);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(JobDataTest.testCasesForAllMatchLower))]
+        public void CalculateSuitabilityAllMatchOrLowerTest(Dictionary<string, int> neededKnowledgeRanges, Dictionary<string, int> employeeKnowledgeRanges)
+        {
+            this.Init(neededKnowledgeRanges, employeeKnowledgeRanges);
+            double suitability = jobData.CalculateSuitability(employee);
+
+            Assert.LessOrEqual(suitability, 1);
+            Assert.GreaterOrEqual(suitability, 0);
+        }
+    }
+}
--- Jobs/Data/JobData.cs
+++ Jobs/Data/JobData.cs
@@ -2,16 +2,27 @@
 using Jobs.Data.WorkingPerson.Employer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Jobs.Data
 {
     public class JobData
     {
+        public JobData(string name, Employer employer)
+        {
+            this.Name = name;
+            this.Employer = employer;
+            this.StartTime = DateTime.Now;
+        }
+
         public string Name { get; }
-        public string Details { get; }
+
+        /// <summary>
+        /// Details of a job. Can be null of Employer giving no details about the job.
+        /// </summary>
+        public string? Details { get; }
 
         // TODO: eventhandler -> set to false -> EndTime = DateTime.Now, null values needs to be initialized
-
         /// <summary>
         /// True if the job is active. False if the job is done.
         /// </summary>
@@ -21,22 +32,59 @@ namespace Jobs.Data
 
         public Employer Employer { get; }
 
+        /// <summary>
+        /// List of <see cref="Employee"/>s working on this job.
+        /// </summary>
         public List<Employee> Employees { get; } = new List<Employee>();
 
         /// <summary>
         /// List of <see cref="Skill"/>s that are needed for this job.
         /// </summary>
-        public List<Skill> NeededSkills { get; set; }
+        public List<Skill> NeededSkills { get; set; } = new List<Skill>();
 
         /// <summary>
-        ///  Returns percentage of Employee.SkillDatas <-> JobData.NeededSkills
-        /// </summary>
+        ///  Returned value will be 1 if all <see cref="Skill.RangeOfKnowledge"/> of <see cref="NeededSkills"/> are matching <paramref name="employee"/>s Skills.<br/>
+        ///  Returned value will be higher than 1 if all <see cref="Skill.RangeOfKnowledge"/> of <see cref="NeededSkills"/> are matching <paramref name="employee"/>s Skills and some of the <paramref name="employee"/>s Skills are higher than needed.<br/>
+        ///  Returned value will be lower than 1 if some <see cref="Skill.RangeOfKnowledge"/> of <see cref="NeededSkills"/> are higher than <paramref name="employee"/>s Skills.<br/>
+        ///  </summary>
         /// <param name="employee"></param>
-        /// <returns>Percentage</returns>
-        public int CalculateEmployeeSuitability(Employee employee)
+        /// <returns></returns>
+        public double CalculateSuitability(Employee employee)
         {
-            //TODO: implementation
-            throw new NotImplementedException();
+            double suitability = 0;
+            List<double> ranges = new List<double>();
+            List<Skill> employeeSkills = employee.ProfessionData.Skills;
+            bool anyLessThan1OrNotInEmployeesSkills = false;
+            for (int i = 0; i < NeededSkills.Count; i++)
+            {
+                Skill neededSkill = this.NeededSkills[i];
+                double range = 0;
+                if (employeeSkills.Any(skill => skill.Name == neededSkill.Name))
+                {
+                    Skill employeeSkill = employeeSkills.First(skill => skill.Name == neededSkill.Name);
+                    range = employeeSkill.RangeOfKnowledge / neededSkill.RangeOfKnowledge;
+                    if(range < 1 && !anyLessThan1OrNotInEmployeesSkills)
+                    {
+                        anyLessThan1OrNotInEmployeesSkills = true;
+                    }
+                }
+                else if(!anyLessThan1OrNotInEmployeesSkills)
+                {
+                    anyLessThan1OrNotInEmployeesSkills = true;
+                }
+                ranges.Add(range);
+            }
+            double rangesSum = 0;
+            for (int i = 0; i < ranges.Count; i++)
+            {
+                if(anyLessThan1OrNotInEmployeesSkills && ranges[i] > 1)
+                {
+                    ranges[i] = 1;
+                }
+                rangesSum += ranges[i];
+            }
+            suitability = rangesSum / ranges.Count;
+            return suitability;
         }
     }
 }
